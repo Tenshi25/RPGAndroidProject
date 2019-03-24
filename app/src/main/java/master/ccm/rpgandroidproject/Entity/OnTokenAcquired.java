99a10db80f571fbdfd42522b8633863557e2a7ba
remig <urbaniec.remi@gmail.com>
2019-03-25 00:16:45 +0100

    test travis 5 : + connexion +OAuth (fonctionne pas)

---
+++ app/src/main/java/master/ccm/rpgandroidproject/Entity/OnTokenAcquired.java
@@ -0,0 +1,37 @@
+package master.ccm.rpgandroidproject.Entity;
+
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerCallback;
+import android.accounts.AccountManagerFuture;
+import android.accounts.AuthenticatorException;
+import android.accounts.OperationCanceledException;
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.io.IOException;
+
+/*private class OnTokenAcquired implements AccountManagerCallback<Bundle> {
+    @Override
+    public void run(AccountManagerFuture<Bundle> result) {
+        // Get the result of the operation from the AccountManagerFuture.
+        Bundle bundle = null;
+        try {
+            bundle = result.getResult();
+        } catch (OperationCanceledException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (AuthenticatorException e) {
+            e.printStackTrace();
+        }
+
+        // The token is a named value in the bundle. The name of the value
+        // is stored in the constant AccountManager.KEY_AUTHTOKEN.
+        String token = bundle.getString(AccountManager.KEY_AUTHTOKEN);
+        /*Intent launch = (Intent) result.getResult().get(AccountManager.KEY_INTENT);
+        if (launch != null) {
+            //startActivityForResult(launch, 0);
+            return;
+        }
+    }
+}*/
--- app/src/main/java/master/ccm/rpgandroidproject/activite/MainActivity.java
+++ app/src/main/java/master/ccm/rpgandroidproject/activite/MainActivity.java
@@ -1,6 +1,8 @@
 package master.ccm.rpgandroidproject.activite;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,11 +10,20 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import master.ccm.rpgandroidproject.Entity.Utilisateur;
 import master.ccm.rpgandroidproject.R;
 import master.ccm.rpgandroidproject.manager.BDDManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    GoogleApiClient mGoogleApiClient;
     private EditText champNom;
     private EditText champPassword;
     private static Utilisateur unUtilisateur = new Utilisateur();
@@ -23,7 +34,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         champNom =  findViewById(R.id.tb_username);
         champPassword =  findViewById(R.id.tb_password);
-
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
     }
 
     public void onClickButtonInscription(View view) {
@@ -44,5 +61,25 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this,"Vous êtes connecté",Toast.LENGTH_SHORT).show();
         Intent monIntent = new Intent (this, pageAccueil.class);
         startActivity(monIntent);
+    }public void onClickGoogleOAuth(View view) {
+        Intent sign = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(sign, 9025);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 9025){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if(result.isSuccess()){
+                GoogleSignInAccount acct = result.getSignInAccount();
+                Log.i("Login", acct.getDisplayName());
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
