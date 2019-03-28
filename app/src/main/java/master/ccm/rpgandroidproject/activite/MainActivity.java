674a2eb82bb4065d78e4fd4fe4d2ccc8e1a80d21
Urbaniec Rémi <urbaniec.remi@gmail.com>
2019-03-28 17:27:17 +0100

    Debut Personnage et stats

--- app/src/main/java/master/ccm/rpgandroidproject/activite/MainActivity.java
+++ app/src/main/java/master/ccm/rpgandroidproject/activite/MainActivity.java
@@ -101,4 +101,5 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     }
 
 
+
 }
--- app/src/main/java/master/ccm/rpgandroidproject/activite/pageAccueil.java
+++ app/src/main/java/master/ccm/rpgandroidproject/activite/pageAccueil.java
@@ -1,15 +1,16 @@
 package master.ccm.rpgandroidproject.activite;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import master.ccm.rpgandroidproject.Entity.StaticUtilisateurInfo;
-import master.ccm.rpgandroidproject.Entity.Utilisateur;
 import master.ccm.rpgandroidproject.R;
-import master.ccm.rpgandroidproject.manager.BDDManager;
+
 
 public class pageAccueil extends AppCompatActivity {
     private TextView untextView;
@@ -22,5 +23,13 @@ public class pageAccueil extends AppCompatActivity {
         String chaineBienvenue ="bienvenue : "+StaticUtilisateurInfo.getInstance().getNom();
         //Log.i("setInfo", StaticUtilisateurInfo.getInstance().getNom());
         untextView.setText(chaineBienvenue);
+    }
+
+    public void onConnectDisconnect(View view) {
+        StaticUtilisateurInfo.getInstance().setId(null);
+        StaticUtilisateurInfo.getInstance().setNom(null);
+        Intent monIntent = new Intent (this, MainActivity.class);
+        startActivity(monIntent);
+
     }
 }
